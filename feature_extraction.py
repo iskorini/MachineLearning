6e9cb3cc86198b72fa955687537045b62dafb016
Federico Schipani <iskorini@gmail.com>
2019-03-02 12:51:25 +0100

    aa

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,6 +5,7 @@ import scipy.io.wavfile as wav
 from sys import exit
 from pathlib import Path
 import pandas as pd
+import csv
 
 def extract_feature_from_wav(path):
     (rate, sig) = wav.read(path)
@@ -17,13 +18,31 @@ def extract_feature_from_wav(path):
     return np.reshape(sphp.feature.extract_derivative_feature(feature)[:, :, 0:2], (log_energy.shape[0], 26))
 
 
-for file_name in glob.iglob('./timit/**/**/**/*CONVERTED.wav'):
-    feature = extract_feature_from_wav(file_name)
-    feature_name = file_name.replace('timit', 'preprocessed_dataset')
-    feature_name = feature_name.replace('wav', 'csv')
-    feature_name = feature_name.replace('CONVERTED', '')
-    Path(feature_name[:''.join(feature_name).rindex('/')]).mkdir(parents=True, exist_ok=True)
-    print(file_name, " -> ", feature_name)
-    pd.DataFrame(feature).to_csv(feature_name)
+def create_file_of_feature(path):
+    for file_name in glob.iglob(path):
+        feature = extract_feature_from_wav(file_name)
+        feature_name = file_name.replace('timit', 'preprocessed_dataset')
+        feature_name = feature_name.replace('wav', 'csv')
+        feature_name = feature_name.replace('CONVERTED', '')
+        Path(feature_name[:''.join(feature_name).rindex('/')]).mkdir(parents=True, exist_ok=True)
+        print(file_name, " -> ", feature_name)
+        pd.DataFrame(feature).to_csv(feature_name)
 
-exit(0)
+
+def create_train_labels(path):
+    print("nulla")
+
+
+if __name__ == '__main__':
+    #create_file_of_feature('./timit/**/**/**/*CONVERTED.wav')
+    file = open('./timit/test/dr1/faks0/sa1.phn', 'r')
+    phoneme = file.read()
+    file.close()
+    file = open('./preprocessed_dataset/test/dr1/faks0/sa1.csv', 'r')
+    feature = pd.read_csv(file)
+    file.close()
+    phoneme = np.array([[e for e in line.split(' ')] for line in phoneme.splitlines()])
+    phoneme[:, 0:2] = (phoneme[:, 0:2].astype(int)) // 16 #convert to ms (sampling rate 16KHz)
+
+    print(feature)
+    exit(0)
