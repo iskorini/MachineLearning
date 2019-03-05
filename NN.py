44bcee12cd3573b3ca4081c4e141e3e3fc1961d1
Federico Schipani <iskorini@gmail.com>
2019-03-05 19:00:02 +0100

    feature extraction for simple model

--- NN.py
+++ NN.py
@@ -1,5 +1,6 @@
 from keras import layers, models
 from keras.utils.np_utils import to_categorical
+from keras.utils import normalize
 from sklearn.preprocessing import LabelEncoder
 import glob
 import pandas as pd
@@ -10,9 +11,13 @@ from operator import itemgetter
 from sys import exit
 from DataGenerator import MyDataGenerator
 
-def CNN_SPR():
-    model = models.Sequential()
-    return model
+def SimpleModel():
+
+def encode_label(label):
+    encoder = LabelEncoder()
+    encoder.fit(train_label.astype(str))
+    train_encoded_labels = encoder.transform(train_label)
+    return to_categorical(train_encoded_labels)
 
 
 if __name__ == '__main__':
@@ -20,16 +25,11 @@ if __name__ == '__main__':
     d = MyDataGenerator('./preprocessed_dataset/train/dr1/**/**.csv')
     train_data, train_label = d.generate_data()
 
-    encoder = LabelEncoder()
-    encoder.fit(train_label.astype(str))
-    train_encoded_labels = encoder.transform(train_label)
-    train_label = to_categorical(train_encoded_labels)
+    train_label = encode_label(train_label)
 
-    #train_data -= train_data.mean(axis=1)
-    #train_data /= train_data.std(axis=1)
+    train_data = normalize(train_data, axis=1, order=2)
 
-    #model = CNN_SPR()
-    #print(model.summary())
-    #history = model.fit(train_data, train_label,
-    #                    epochs=5, batch_size=256, validation_split=0.2)
+    model = CNN_SPR()
+    print(model.summary())
+    history = model.fit(train_data, train_label, epochs=5, batch_size=128, validation_split=0.2)
     #exit(0)
--- feature_extraction.py
+++ feature_extraction.py
@@ -21,36 +21,31 @@ def extract_feature_from_wav(path):
 
 def create_file_of_feature(path):
     for file_name in glob.iglob(path):
-
         feature_name = file_name.replace('timit', 'preprocessed_dataset_LMFE')
         feature_name = feature_name.replace('.wav', '.csv')
         feature_name = feature_name.replace('CONVERTED', '')
         Path(feature_name[:''.join(feature_name).rindex('/')]).mkdir(parents=True, exist_ok=True)
         print(file_name, " -> ", feature_name)
         extracted_feature = extract_feature_from_wav(file_name)
-        for i in range(0, 3):
-            feature = pd.DataFrame(extracted_feature[:, :, i])
-            feature.rename(index=str, columns={'Unnamed: 0': 'frame'})
-            feature['start_frame'] = np.arange(len(feature))
-            feature['start_frame'] = feature['start_frame'] * FRAME_STRIDE * 1000
-            feature['end_frame'] = feature['start_frame'] + FRAME_LENGTH * 1000
-            phoneme_name = file_name.replace('CONVERTED', '')
-            phoneme_name = phoneme_name.replace('wav', 'phn')
-            file = open(phoneme_name, 'r')
-            phoneme = file.read()
-            file.close()
-            phoneme = np.array([[e for e in line.split(' ')] for line in phoneme.splitlines()])
-            phoneme[:, 0:2] = (phoneme[:, 0:2].astype(int)) // 16  # convert to ms (sampling rate 16KHz)
-            feature['phoneme'] = 'h#'
-            for row in phoneme:
-                feature.loc[feature['start_frame'] > int(row[0]), 'phoneme'] = row[2]
-            feature.to_csv(feature_name.replace('.csv', '-'+str(i)+'.csv'))
-
+        feature = pd.DataFrame(np.concatenate((extracted_feature[:,:,0],
+                                            extracted_feature[:,:,1],
+                                            extracted_feature[:,:,2]), axis=1))
+        feature.rename(index=str, columns={'Unnamed: 0': 'frame'})
+        feature['start_frame'] = np.arange(len(feature))
+        feature['start_frame'] = feature['start_frame'] * FRAME_STRIDE * 1000
+        feature['end_frame'] = feature['start_frame'] + FRAME_LENGTH * 1000
+        phoneme_name = file_name.replace('CONVERTED', '')
+        phoneme_name = phoneme_name.replace('wav', 'phn')
+        file = open(phoneme_name, 'r')
+        phoneme = file.read()
+        file.close()
+        phoneme = np.array([[e for e in line.split(' ')] for line in phoneme.splitlines()])
+        phoneme[:, 0:2] = (phoneme[:, 0:2].astype(int)) // 16  # convert to ms (sampling rate 16KHz)
+        feature['phoneme'] = 'h#'
+        for row in phoneme:
+            feature.loc[feature['start_frame'] > int(row[0]), 'phoneme'] = row[2]
+        feature.to_csv(feature_name)
 
 if __name__ == '__main__':
     create_file_of_feature('./timit/**/dr1/**/**CONVERTED.wav')
-    create_file_of_feature('./timit/**/dr2/**/**CONVERTED.wav')
-    create_file_of_feature('./timit/**/dr3/**/**CONVERTED.wav')
-    create_file_of_feature('./timit/**/dr4/**/**CONVERTED.wav')
-    create_file_of_feature('./timit/**/dr5/**/**CONVERTED.wav')
     exit(0)
