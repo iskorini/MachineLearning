f6de00db3cfd8d08ca3f39e741180dc4b2eb6b43
Federico Schipani <iskorini@gmail.com>
2019-03-07 20:27:00 +0100

    classification of 'p', 't' and 'k'

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,8 +1,6 @@
 import glob
 import pandas as pd
 import numpy as np
-from itertools import groupby
-from operator import itemgetter
 
 
 class MyDataGenerator:
@@ -10,11 +8,13 @@ class MyDataGenerator:
     def __init__(self, path):
         self.__path = path
 
-    def generate_data(self):
+    def generate_data(self, phoneme_list=None):
         label = np.empty(0)
         data = np.empty((0,120))
         for file_name in glob.iglob(self.__path):
             file = pd.read_csv(file_name).rename(columns={'Unnamed: 0': 'frame'})
+            if phoneme_list is not None:
+                file = file.loc[file['phoneme'].isin(phoneme_list)]
             label = np.concatenate((label, np.array(file.iloc[:, -1])))
             data = np.concatenate((data, np.array(file.iloc[:, 1:121])))
         return data, label
--- NN.py
+++ NN.py
@@ -12,24 +12,51 @@ from sys import exit
 from DataGenerator import MyDataGenerator
 
 def SimpleModel():
+    model = models.Sequential()
+    model.add(
+        layers.Dense(120 ,input_shape=(120,))
+    )
+    model.add(
+        layers.Dense(120)
+    )
+    model.add(
+        layers.Dense(3)
+    )
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='rmsprop',
+        metrics=['acc']
+    )
+    return model
+
+def plot_value(epochs, history):
+    plt.figure()
+    plt.plot(range(0, epochs), history.history['val_loss'], 'bo', label = "val loss")
+    plt.plot(range(0, epochs), history.history['val_acc'], 'b', label="val acc")
+    plt.legend()
+    plt.show()
+
 
 def encode_label(label):
     encoder = LabelEncoder()
-    encoder.fit(train_label.astype(str))
-    train_encoded_labels = encoder.transform(train_label)
+    encoder.fit(label.astype(str))
+    train_encoded_labels = encoder.transform(label)
     return to_categorical(train_encoded_labels)
 
 
 if __name__ == '__main__':
-
-    d = MyDataGenerator('./preprocessed_dataset/train/dr1/**/**.csv')
-    train_data, train_label = d.generate_data()
+    phoneme_list = ['p', 't', 'k']
+    d = MyDataGenerator('./preprocessed_dataset_LMFE/train/**/**/**.csv')
+    train_data, train_label = d.generate_data(phoneme_list)
+    d = MyDataGenerator('./preprocessed_dataset_LMFE/test/**/**/**.csv')
+    test_data, test_label = d.generate_data(phoneme_list)
 
     train_label = encode_label(train_label)
-
+    test_label = encode_label(test_label)
     train_data = normalize(train_data, axis=1, order=2)
-
-    model = CNN_SPR()
+    test_data = normalize(test_data, axis=1, order=2)
+    model = SimpleModel()
     print(model.summary())
-    history = model.fit(train_data, train_label, epochs=5, batch_size=128, validation_split=0.2)
+    history = model.fit(train_data, train_label, epochs=20, batch_size=64, validation_split=0.3)
+    print(model.evaluate(test_data, test_label))
     #exit(0)
